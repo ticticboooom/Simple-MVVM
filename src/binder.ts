e2b5db730cf32ad2fdc8f21c2e28793f285b0377
Kyle Cox <kyle.benjamin.cox@outlook.com>
2018-08-15 09:30:06 +0100

    Removing Unneeded iteration inside binder class

--- src/binder.ts
+++ src/binder.ts
@@ -3,26 +3,24 @@ import { Observable } from './observable';
 import { BindingModel } from './models/binding-model';
 import { BindingConstants } from './constants/binding-constants';
 export class Binder {
-    private eventDictionary: { [id: string]: string[]; } = {};
-    constructor() {
-        this.eventDictionary[BindingConstants.valueBinding] = ["input"];
-    }
-    public bind(vm: any, element: Element, model: BindingModel) {
-        let eventsArr = this.eventDictionary[model.keyword];
-        model.initExpression(vm, element);
-        for (let i = 0; i < eventsArr.length; i++) {
-            const event = eventsArr[i];
-            element.addEventListener(event, (event) => {
-                model.setExpression(vm, element);
-            });
+	private eventDictionary: { [id: string]: string[] } = {};
+	constructor() {
+		this.eventDictionary[BindingConstants.valueBinding] = ['input'];
+	}
+	public bind(vm: any, element: Element, model: BindingModel) {
+		let eventsArr = this.eventDictionary[model.keyword];
+		model.initExpression(vm, element);
+		for (let i = 0; i < eventsArr.length; i++) {
+			const event = eventsArr[i];
+			element.addEventListener(event, event => {
+				model.setExpression(vm, element);
+			});
         }
-
-        for (const key in vm) {
-            if (vm.hasOwnProperty(key) && element.getAttribute(DOMConstants.boundNameAttribute) == key) {
-                vm[key].setExpression = () => {
-                    model.getExpression(vm, element);
-                }
-            }
-        }
-    }
+        const key = element.getAttribute(DOMConstants.boundNameAttribute);
+		if (vm.hasOwnProperty(key)) {
+			vm[key].setExpression = () => {
+				model.getExpression(vm, element);
+			};
+		}
+	}
 }
